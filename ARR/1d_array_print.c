a4b4d8d5b285326a5f9877f64513d47dbc75bade
Roshan4560 <roshankumar86377@gmail.com>
2025-06-03 05:48:54 +0530

    Add files via upload

---
+++ ARR/1d_array_print.c
@@ -0,0 +1,15 @@
+/* WAP to enter the array and print the array in the screen */
+
+# include<stdio.h>
+int main(){
+    int a[3];
+    printf("enter element\n");
+    for(int i=0; i<3; i++){
+        scanf("%d", &a[i]);
+    }
+    printf("array element \n");
+    for(int i=0; i<3; i++){
+        printf("%d\n", a[i]);
+    }
+    return 0;
+}
---
+++ ARR/selection_sort.c
@@ -0,0 +1,27 @@
+/* WAP using selection sort method to sort the array in the ascending order */
+
+# include<stdio.h>
+int main(){
+    int n,i,j,temp;
+    int a[10];
+    printf("n=\n");
+    scanf("%d", &n);
+    printf("Enter element\n");
+    for(int i=0; i<n; i++){
+        scanf("%d", &a[i]);
+    }
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){    // similarly in decending order//
+            if(a[j]<a[i]){          // if(a[j]>a[i])                //
+                temp=a[i];
+                a[i]=a[j];
+                a[j]=temp;
+            }
+        }
+    }
+    printf("arrangements are\n");
+    for(i=0; i<n; i++){
+        printf("%d\n", a[i]);
+    }
+    return 0;
+}
